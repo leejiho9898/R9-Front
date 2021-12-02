b3ccc49333dba94cb1e3d3824d0f271d801c2870
이지호 <cctv1998@naver.com>
2021-12-02 22:17:48 +0900

    Feat : boxes (component 감싸는 Paper)

---
+++ src/styles/Boxes.tsx
@@ -0,0 +1,19 @@
+import { Container, Paper } from "@mui/material";
+import { styled } from "@mui/styles";
+
+/** 넓은 화면 (기본값) */
+export const BasicBox = styled(Paper)({
+  marginX: "auto",
+  maxWidth: "md",
+  padding: "25px",
+});
+
+/** 좁은 화면 */
+export const ContainerBox = styled(Container)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  padding: 10,
+});

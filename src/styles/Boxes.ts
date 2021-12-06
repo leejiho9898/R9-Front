aa0464e86f04986393876e76f5646f06d72e536b
이지호 <cctv1998@naver.com>
2021-12-06 17:23:57 +0900

    Feat : TypoQue, TypoAns

--- src/styles/Boxes.ts
+++ src/styles/Boxes.ts
@@ -1,8 +1,15 @@
-import { Container, Paper } from "@mui/material";
+import { Container, Paper, Typography } from "@mui/material";
 import { styled } from "@mui/styles";
 
 /** 넓은 화면 (기본값) */
 export const BasicBox = styled(Paper)({
+  marginX: "auto",
+  maxWidth: "md",
+  padding: "20px",
+});
+
+/** 넓은 배경 없는 화면 (기본값) */
+export const BasicDarkBox = styled(Container)({
   marginX: "auto",
   maxWidth: "md",
   padding: "20px",
@@ -14,6 +21,15 @@ export const ContainerBox = styled(Container)({
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  textAlign: "center",
   padding: 10,
+});
+
+export const TypoQue = styled(Typography)({
+  width: "6rem",
+  fontWeight: "bold",
+  fontSize: "13px",
+});
+
+export const TypoAns = styled(Typography)({
+  fontSize: "12px",
 });

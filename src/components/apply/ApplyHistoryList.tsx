ed63c48405f5c8641106119ab769e2888fbc5521
이지호 <cctv1998@naver.com>
2021-12-06 17:22:49 +0900

    Refactor : 지원 목록 요소 뿌리는방식

--- src/components/apply/ApplyHistoryList.tsx
+++ src/components/apply/ApplyHistoryList.tsx
@@ -1,98 +1,73 @@
 import { Card, Typography, Stack, Button } from "@mui/material";
 import { Box } from "@mui/system";
+import moment from "moment";
 import React from "react";
+import useHandleApply from "~/hooks/apply/useHandleApply";
+import { useMyApplysEffect } from "~/hooks/apply/useMyApplysEffect";
+import { TypoAns, TypoQue } from "~/styles/Boxes";
+import { JobStatus } from "~/types/enums";
 export interface ApplyerHistoryListItem {
   name: string;
 }
-export const ApplyerHistoryLists: ApplyerHistoryListItem[] = [
-  { name: "지원내역" },
-  { name: "지원일" },
-  { name: "게재상태" },
-];
 const ApplyHistoryList = () => {
+  const { applys } = useMyApplysEffect();
+  console.log(applys);
+  const { deleteApply } = useHandleApply();
   return (
     <>
       <Typography align="center" variant="h5" py={2}>
         지원 내역
       </Typography>
-      <Card
-        sx={{
-          p: 1.5,
-          border: "1px solid #d5d3d3",
-          marginBottom: 2,
-        }}
-      >
-        <Stack direction="row" spacing={2} justifyContent="center">
-          {/* 지원자 정보 */}
-          <Box sx={{ width: "100%" }}>
-            <Typography gutterBottom variant="subtitle2" component="div">
-              홍길동 (65세)
-            </Typography>
-            <Box p={1} sx={{ textAlign: "left" }}>
-              {ApplyerHistoryLists.map(
-                (ApplyerHistoryList: ApplyerHistoryListItem, index) => (
-                  <Stack direction="row" spacing={1} key={index}>
-                    <Typography
-                      sx={{
-                        width: "6rem",
-                        fontWeight: "bold",
-                        fontSize: "13px",
-                      }}
-                    >
-                      {ApplyerHistoryList.name} :
-                    </Typography>
-                    <Typography sx={{ fontSize: "12px" }}>
-                      어쩌구 저쩌구 ㅋㅋ 어쩌구 저쩌구 ㅋㅋ 어쩌구 저쩌구 ㅋㅋ
-                    </Typography>
-                  </Stack>
-                )
-              )}
+      {applys.map((apply) => (
+        <Card
+          sx={{
+            p: 1.5,
+            border: "1px solid #d5d3d3",
+            marginBottom: 2,
+          }}
+          key={apply.id}
+        >
+          <Stack direction="row" spacing={2} justifyContent="center">
+            {/* 지원자 정보 */}
+            <Box sx={{ width: "100%" }}>
+              <Typography gutterBottom variant="subtitle2" component="div">
+                {apply.job.title} (
+                {moment(apply.createdAt).format("YYYY-MM-DD")} 지원)
+              </Typography>
+              <Box p={1} sx={{ textAlign: "left" }}>
+                <Stack direction="row" spacing={1}>
+                  <TypoQue>지원 추가정보 :</TypoQue>
+                  <TypoAns>{apply.moreDetail}</TypoAns>
+                </Stack>
+                <Stack direction="row" spacing={1}>
+                  <TypoQue>담당 업무 :</TypoQue>
+                  <TypoAns>{apply.job.workType}</TypoAns>
+                </Stack>
+                <Stack direction="row" spacing={1}>
+                  <TypoQue>게재상태 :</TypoQue>
+                  <TypoAns>
+                    {apply.job.status === JobStatus.ACTIVATE
+                      ? "모집중"
+                      : apply.job.status === JobStatus.INACTIVATE
+                      ? "모집완료"
+                      : "모집중지"}
+                  </TypoAns>
+                </Stack>
+              </Box>
+
+              <Button
+                sx={{ float: "right" }}
+                variant="outlined"
+                onClick={() => {
+                  deleteApply(apply.id);
+                }}
+              >
+                지원 취소
+              </Button>
             </Box>
-            <Button sx={{ float: "right" }} variant="outlined">
-              지원 취소
-            </Button>
-          </Box>
-        </Stack>
-      </Card>
-      <Card
-        sx={{
-          p: 1.5,
-          border: "1px solid #d5d3d3",
-          marginBottom: 2,
-        }}
-      >
-        <Stack direction="row" spacing={2} justifyContent="center">
-          {/* 지원자 정보 */}
-          <Box sx={{ width: "100%" }}>
-            <Typography gutterBottom variant="subtitle2" component="div">
-              홍길동 (65세)
-            </Typography>
-            <Box p={1} sx={{ textAlign: "left" }}>
-              {ApplyerHistoryLists.map(
-                (ApplyerHistoryList: ApplyerHistoryListItem, index) => (
-                  <Stack direction="row" spacing={1} key={index}>
-                    <Typography
-                      sx={{
-                        width: "6rem",
-                        fontWeight: "bold",
-                        fontSize: "13px",
-                      }}
-                    >
-                      {ApplyerHistoryList.name} :
-                    </Typography>
-                    <Typography sx={{ fontSize: "12px" }}>
-                      어쩌구 저쩌구 ㅋㅋ 어쩌구 저쩌구 ㅋㅋ 어쩌구 저쩌구 ㅋㅋ
-                    </Typography>
-                  </Stack>
-                )
-              )}
-            </Box>
-            <Button sx={{ float: "right" }} variant="outlined">
-              지원 취소
-            </Button>
-          </Box>
-        </Stack>
-      </Card>
+          </Stack>
+        </Card>
+      ))}
     </>
   );
 };

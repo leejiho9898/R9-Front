c3b561c51df45dbcea4a9bcaaf4f82856dc4f003
이지호 <cctv1998@naver.com>
2021-12-11 23:09:47 +0900

    Feat : findCategorysAPI

--- src/libs/api/hashtag.ts
+++ src/libs/api/hashtag.ts
@@ -6,3 +6,8 @@ export const findHashtagsAPI = async () => {
   const response = await client.get(url.HASHTAGS);
   return response.data;
 };
+
+export const findCategorysAPI = async () => {
+  const response = await client.get(`${url.HASHTAGS}/category`);
+  return response;
+};

e41bc0e47ee8f95939d225d9abad175561c7d50d
이지호 <cctv1998@naver.com>
2021-12-16 21:48:00 +0900

    Feat : findApplyByIdAPI

--- src/libs/api/apply.ts
+++ src/libs/api/apply.ts
@@ -14,3 +14,9 @@ export const deleteApplyById = async (id: number) => {
   const response = await client.delete(`${url.APPLYS}/${id}`);
   return response.data;
 };
+
+/** 특정 지원서 불러오기*/
+export const findApplyByIdAPI = async (id: number) => {
+  const response = await client.get(`${url.APPLYS}/${id}`);
+  return response.data;
+};

88b910b5c161412c7a309fdb0679aaeb01aa68c3
이지호 <cctv1998@naver.com>
2021-12-16 21:48:46 +0900

    Feat : 지원자 detail Page

--- src/pages/applyer/[jobid]/[applyid]/index.tsx
+++ src/pages/applyer/[jobid]/[applyid]/index.tsx
@@ -5,50 +5,70 @@ import {
   Box,
   Card,
   TextField,
-  Container,
   Button,
+  Stack,
+  Avatar,
 } from "@mui/material";
-import { Person } from "@mui/icons-material";
+
 import { styled } from "@mui/system";
 import { JobTags } from "~/components/application/Jobtags";
 import { ContainerBox } from "~/styles/Boxes";
+import useApplyerEffect from "~/hooks/apply/useApplyerEffect";
+import { useRouter } from "next/router";
+import { Gender } from "~/types/user";
 
 const ProfileImage = styled(IconButton)({
-  background: "#796f6f",
-  borderRadius: "50",
   padding: 30,
   color: "#fff",
 });
 
 // 지원자 사장님에게 보낸 지원서
 const hashtages1 = ["문서 작업", "매장 관리", "운전 가능"];
-const hashtages2 = ["주3회", "평일 오전", "오픈시간"];
-const hashtages3 = ["마포구", "종로구", "서울시"];
 
 // 타입정의 해야함
 const JobApplyPage2: NextPage = () => {
+  const router = useRouter();
+  const { applyid } = router.query;
+  console.log(applyid);
+  const { apply } = useApplyerEffect(applyid);
+  console.log(apply);
   return (
     <ContainerBox>
       <Card sx={{ p: 4, textAlign: "center" }}>
         {/* 지원자 정보 */}
         <Box>
           <ProfileImage>
-            <Person fontSize="large" />
+            <Avatar
+              sx={{ width: 80, height: 80 }}
+              alt="profile"
+              src={apply?.user.profileImage}
+            />
           </ProfileImage>
           <Typography mt={2} gutterBottom variant="subtitle2" component="div">
-            홍길동 (65세)
+            {apply?.user.name}
           </Typography>
         </Box>
-        <Box p={2} sx={{ width: "80%" }}>
-          <Typography variant="body2">주소</Typography>
-          <Typography variant="body2">휴대전화</Typography>
-          <Typography variant="body2">메일주소</Typography>
-        </Box>
+        <Stack spacing={1}>
+          <Typography variant="body1">주소 : {apply?.user.address}</Typography>
+          <Typography variant="body1">
+            생년월일 :{apply?.user.dateOfBirth}
+          </Typography>
+          <Typography variant="body1">
+            메일주소 : {apply?.user.email}
+          </Typography>
+          <Typography variant="body1">
+            성별 :{apply?.user.gender === Gender.MALE ? "남성" : "여성"}
+          </Typography>
+        </Stack>
         {/* 태그 */}
         <Box>
-          <JobTags title="나의 장점은?" type={hashtages1} />
-          <JobTags title="선호 시간대" type={hashtages2} />
-          <JobTags title="근무 가능 지역?" type={hashtages3} />
+          {apply?.user.useHashtags === undefined ? (
+            <Typography variant="body1">
+              해당 유저는 핵심 키워드를 입력하지 않았습니다.
+            </Typography>
+          ) : (
+            <JobTags title="지원자 핵심 키워드" type={hashtages1} />
+          )}
         </Box>
 
         <Box px={2}>

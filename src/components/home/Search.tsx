34e4146d58475d86ba68b0f12efb4ccb3430fb8f
이지호 <cctv1998@naver.com>
2021-12-20 02:39:18 +0900

    Refactor : Search 컴포넌트 분리

--- src/components/home/Search.tsx
+++ src/components/home/Search.tsx
@@ -1,56 +1,28 @@
-import React, { useState } from "react";
-import { Typography, FormControl, TextField, Chip, Stack } from "@mui/material";
-import { styled } from "@mui/system";
-
-const SearchWrapper = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  width: "70%",
-  marginBottom: 2,
-});
-
-const StyledStack = styled(Stack)({
-  display: "flex",
-  alignItems: "center",
-});
-
-const HASHTAGS = ["돌봄", "주3회", "시니어 일자리", "당일 알바"];
+import React from "react";
+import { TextField, Stack, Button } from "@mui/material";
+import { useSearchForm } from "~/hooks/search/useSearchForm";
 
 const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const { search, onChangeSearch, onSearch } = useSearchForm();
 
   return (
-    <SearchWrapper>
-      {/* 검색창 */}
-      <Typography mt={4} gutterBottom variant="h6" component="div">
-        일자리 검색
-      </Typography>
-      <FormControl sx={{ maxWidth: 800 }}>
-        <StyledStack py={1} spacing={1} direction="row" justifyContent="center">
-          <TextField
-            size="small"
-            focused
-            placeholder="검색어를 입력해주세요"
-            value={searchTerm}
-            onChange={(event) => {
-              setSearchTerm(event.target.value);
-            }}
-          />
-        </StyledStack>
-
-        {/* 추천 검색어 */}
-        {/* <Box mt={1}> */}
-        <Stack direction="row" spacing={1} justifyContent="center">
-          {/* key값으로 index넣으면 안됨 */}
-          {HASHTAGS.map((item) => (
-            <Chip key={item} label={item} />
-          ))}
-        </Stack>
-        {/* </Box> */}
-      </FormControl>
-    </SearchWrapper>
+    <Stack direction="row" spacing={0.25} mt="5" justifyContent="center">
+      <TextField
+        sx={{
+          width: "80%",
+          backgroundColor: "white",
+          borderRadius: 10,
+        }}
+        size="small"
+        focused
+        placeholder="아르바이트를 검색해주세요"
+        value={search}
+        onChange={onChangeSearch}
+      />
+      <Button variant="contained" sx={{ width: "3rem" }} onClick={onSearch}>
+        검색
+      </Button>
+    </Stack>
   );
 };
 

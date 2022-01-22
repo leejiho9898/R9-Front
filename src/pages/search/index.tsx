16042bc93deb3a8ea36f8cdde867d5217486cd95
이지호 <cctv1998@naver.com>
2022-01-22 23:25:39 +0900

    Feat : SearchPage pagenation

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NextPage } from "next";
-import { Typography } from "@mui/material";
+import { Button, Pagination, Typography } from "@mui/material";
 import JobCard from "~/components/home/JobCard";
 import { BasicDarkBox } from "~/styles/Boxes";
 import { useRouter } from "next/router";
@@ -9,16 +9,29 @@ import AdvancedSearch from "~/components/home/AdvancedSearch";
 
 const SearchPage: NextPage = () => {
   const router = useRouter();
-  const { title } = router.query;
-  const { jobs } = useJobSearchEffect(title);
+  const { jobs, totalPage, pageNo, setPageNo } = useJobSearchEffect(
+    router.query
+  );
   return (
-    <BasicDarkBox>
-      <Typography sx={{ textAlign: "center", margin: 2 }} variant="h6">
-        일자리 검색
-      </Typography>
-      <AdvancedSearch />
-      <JobCard data={jobs} />
-    </BasicDarkBox>
+    <>
+      <BasicDarkBox>
+        <Typography sx={{ textAlign: "center", margin: 2 }} variant="h6">
+          일자리 검색
+        </Typography>
+        <AdvancedSearch />
+        <JobCard data={jobs} />
+      </BasicDarkBox>
+      <Pagination
+        sx={{ display: "flex", justifyContent: "center", mt: 5 }}
+        count={totalPage}
+        page={pageNo}
+        color="primary"
+        onChange={(e: React.ChangeEvent<unknown>, value: number) => {
+          setPageNo(value);
+          window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+        }}
+      />
+    </>
   );
 };
 

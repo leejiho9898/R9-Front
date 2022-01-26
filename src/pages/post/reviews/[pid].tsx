42124bba08b5223848ba3804ea2822eaa0652b40
이지호 <cctv1998@naver.com>
2022-01-26 17:00:16 +0900

    Style : ReviewPostPage

--- src/pages/post/reviews/[pid].tsx
+++ src/pages/post/reviews/[pid].tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Card, Container, styled } from "@mui/material";
 import ReviewPostEditor from "src/components/review/ReviewPostEditor";
-import { BasicBox } from "~/styles/Boxes";
+import { BasicDarkBox } from "~/styles/Boxes";
 
 const ReviewPostPage = () => {
   return (
     <>
-      <BasicBox>
+      <BasicDarkBox>
         <Card sx={{ p: 4 }}>
           <ReviewPostEditor />
         </Card>
-      </BasicBox>
+      </BasicDarkBox>
     </>
   );
 };
